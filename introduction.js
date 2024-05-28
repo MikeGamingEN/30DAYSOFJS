aa11433a962eb1c23c0ce225a9d1f3abf09eba66
Michael Gbuwe <akposgbuwe@gmail.com>
2024-05-29 00:56:55 +0100

    Day 4.2

--- introduction.js
+++ introduction.js
@@ -1,3 +1,28 @@
+
 console.log('Welcome to 30DaysOfJavaScript')
 
+
 console.log('working on thiry days of Js')
+
+let count = 0
+console.log(count++)
+console.log(count)
+
+let number = 5
+number > 0
+  ? console.log(`${number} is a positive number`)
+  : console.log(`${number} is a negative number`)
+number = -5
+
+number > 0
+  ? console.log(`${number} is a positive number`)
+  : console.log(`${number} is a negative number`)
+
+  console.log(5 + 4 * 3); // 3 + 20
+// Expected output: 23
+
+console.log(4 * 5 ** 2); // 4 * 9
+// Expected output: 36
+
+const agree = confirm('Are you sure you like to delete? ')
+console.log(agree) // result will be true or false based on what you click on the dialog box
