ce425a4cff4484d33caf02cde9ff1157333409a3
Michael Gbuwe <akposgbuwe@gmail.com>
2024-05-29 01:19:52 +0100

    Day 4.2.2

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,21 @@
 console.log('Hello, World!')
+
+const now = new Date()
+console.log(now.getUTCHours())
+
+console.log(now.getSeconds())
+
+const allSeconds = Date.now() //
+console.log(allSeconds) // 1578092201341, this is the number of seconds passed from January 1, 1970 to January 4, 2020 00:56:41
+
+const timeInSeconds = new Date().getTime()
+console.log(allSeconds == timeInSeconds) // true
+
+//const now = new Date()
+const year = now.getFullYear() // return year
+const month = now.getMonth() + 1 // return month(0 - 11)
+const date = now.getDate() // return date (1 - 31)
+const hours = now.getHours() // return number (0 - 23)
+const minutes = now.getMinutes() // return number (0 -59)
+
+console.log(`${date}/${month}/${year} ${hours}:${minutes}`)
