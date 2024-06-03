462bbb3394df088ab22823411910a5f1d9869f8f
Michael Gbuwe <akposgbuwe@gmail.com>
2024-06-03 22:59:56 +0100

    Day 5.4

--- introduction.js
+++ introduction.js
@@ -26,3 +26,36 @@ console.log(4 * 5 ** 2); // 4 * 9
 
 //const agree = confirm('Are you sure you like to delete? ')
 //console.log(agree) // result will be true or false based on what you click on the dialog box
+
+let txt =
+  'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+const words = txt.split(' ')
+
+console.log(words)
+
+const fruits = ['banana', 'orange', 'mango', 'lemon']
+let firstFruit = fruits[0] // we are accessing the first item using its index
+
+console.log(firstFruit) // banana
+
+secondFruit = fruits[1]
+console.log(secondFruit) // orange
+
+let lastFruit = fruits[3]
+console.log(lastFruit) // lemon
+// Last index can be calculated as follows
+
+//let lastIndex = fruits.length - 1
+//lastFruit = fruits[lastIndex]
+
+//console.log(lastFruit)  // lemon
+
+const numbers = [0, 3.14, 9.81, 37, 98.6, 100]  // set of numbers
+
+console.log(numbers.length)  // => to know the size of the array, which is 6
+console.log(numbers)         // -> [0, 3.14, 9.81, 37, 98.6, 100]
+console.log(numbers[0])      //  -> 0
+console.log(numbers[5])      //  -> 100
+
+let lastIndex = numbers.length - 1;
+console.log(numbers[lastIndex]) // -> 100
---
+++ judge.js
@@ -0,0 +1,118 @@
+const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+  ] // List of web technologies
+  
+  console.log(webTechs)        // all the array items
+  console.log(webTechs.length) // => to know the size of the array, which is 7
+  console.log(webTechs[0])     //  -> HTML
+  console.log(webTechs[6])     //  -> MongoDB
+
+//const lastIndex = webTechs.length - 1
+//console.log(webTechs[lastIndex])
+/*
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ] // List of countries
+  
+ console.log(countries)      // -> all countries in array
+  console.log(countries[0])   //  -> Albania
+  console.log(countries[10])  //  -> Kenya
+*/
+ // let lastIndex = countries.length - 1;
+ // console.log(countries[lastIndex]) //  -> Kenya
+
+  const shoppingCart = [
+    'Milk',
+    'Mango',
+    'Tomato',
+    'Potato',
+    'Avocado',
+    'Meat',
+    'Eggs',
+    'Sugar'
+  ] // List of food products
+  
+  console.log(shoppingCart) // -> all shoppingCart in array
+  console.log(shoppingCart[0]) //  -> Milk
+  console.log(shoppingCart[7]) //  -> Sugar
+  
+ // let lastIndex = shoppingCart.length - 1;
+ // console.log(shoppingCart[lastIndex]) //  -> Sugar
+
+//const numbers = [1, 2, 3, 4, 5]
+//numbers[0] = 10      // changing 1 at index 0 to 10
+//numbers[1] = 20      // changing  2 at index 1 to 20
+  
+//console.log(numbers) // [10, 20, 3, 4, 5]
+  
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+]
+  
+countries[0] = 'Afghanistan'  // Replacing Albania by Afghanistan
+let lastIndex = countries.length - 1
+countries[lastIndex] = 'Korea' // Replacing Kenya by Korea
+  
+console.log(countries)
+
+const arr = Array() // creates an an empty array
+console.log(arr)
+
+const sevenEmptyValues = Array(7) // it creates eight empty values
+console.log(sevenEmptyValues) // [empty x 8]
+
+const eightXvalues = Array(8).fill('X')
+console.log(eightXvalues)
+
+const four4values = Array(4).fill(4)
+console.log(four4values)
+
+const firstList = [1, 2, 3]
+const secondList = [4, 5, 6]
+const thirdList = firstList.concat(secondList)
+
+console.log(thirdList)
+
+//const fruits = ['banana', 'orange', 'mango', 'lemon']                 // array of fruits
+const vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot'] // array of vegetables
+//const fruitsAndVegetables = fruits.concat(vegetables)                 // concatenate the two arrays
+
+//console.log(fruitsAndVegetables)
+
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers.indexOf(6))
+
+const fruits = ['banana', 'orange', 'mango', 'lemon']
+let index = fruits.indexOf('apple')  // 0
+
+if(index === -1){
+   console.log('This fruit does not exist in the array')  
+} else {
+    console.log('This fruit does exist in the array')
+}
