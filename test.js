fa8ccd6f62244d05f385d3f0fc56ca724e41aab6
Michael Gbuwe <akposgbuwe@gmail.com>
2024-06-23 22:49:35 +0100

    7.2

--- test.js
+++ test.js
@@ -104,3 +104,16 @@ do {
   i++
 } while (i <= 5)
 
+// function declaration
+​
+function sumAllNums() {
+  let sum = 0
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i]
+  }
+  return sum
+}
+
+console.log(sumAllNums(1, 2, 3, 4)) // 10
+console.log(sumAllNums(10, 20, 13, 40, 10))  // 93
+console.log(sumAllNums(15, 20, 30, 25, 10, 33, 40))  // 173
