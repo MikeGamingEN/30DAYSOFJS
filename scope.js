07b1c743bab88413659692f38b6e679f4a164288
Michael Gbuwe <akposgbuwe@gmail.com>
2024-07-10 22:38:29 +0100

    8.4

--- scope.js
+++ scope.js
@@ -78,7 +78,7 @@ const person = {
 
 console.log(person.age)
 console.log(person['phone number'])
-
+/*
 person.nationality = 'Canadian'
 person.country = 'Spain'
 person.title = 'Student'
@@ -108,5 +108,78 @@ console.log(values)
 const entries = Object.entries(copyPerson)
 console.log(entries)
 console.log(copyPerson.hasOwnProperty('name'))
+*/
+const dog = {
+    name: 'Billy',
+    legs: 4,
+    color: 'Brown',
+    age: 6,
+    bark: 'woof woof'
+}
+console.log(dog.name)
+console.log(dog.legs)
+console.log(dog.color)
+dog.breed = 'Wolfgang'
 
-const dog = {}
+const users = {
+    Alex: {
+      email: 'alex@alex.com',
+      skills: ['HTML', 'CSS', 'JavaScript'],
+      age: 20,
+      isLoggedIn: false,
+      points: 30
+    },
+    Asab: {
+      email: 'asab@asab.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+      age: 25,
+      isLoggedIn: false,
+      points: 50
+    },
+    Brook: {
+      email: 'daniel@daniel.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+      age: 30,
+      isLoggedIn: true,
+      points: 50
+    },
+    Daniel: {
+      email: 'daniel@alex.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    },
+    John: {
+      email: 'john@john.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+      age: 20,
+      isLoggedIn: true,
+      points: 50
+    },
+    Thomas: {
+      email: 'thomas@thomas.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    },
+    Paul: {
+      email: 'paul@paul.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    }
+}
+let userWithMostSkills = ''
+let maxSkills = 0
+
+for (const user in users) {
+    if (users[user].skills.length > maxSkills) {
+        maxSkills = users[user].skills.length
+        userWithMostSkills = user
+    }
+}
+
+console.log('User with the most skills:', userWithMostSkills)
